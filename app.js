9f0711eeb595a8f41453ce74a7acc6c1e5841f69
mhmdrameezsmb <110522603+mhmdrameezsmb@users.noreply.github.com>
2024-07-30 17:03:27 +0530

    added to admin panel and service worekr

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
@@ -9,10 +10,20 @@ const app = express();
 mongoose.connect('mongodb+srv://rameez:0@cluster0.xnqwvnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0l', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
 });
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (including service-worker.js)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use the admin routes
